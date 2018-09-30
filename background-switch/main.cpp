c4e770b0505c4c096f01a51f998c19b87ab8a0c8
Yuri Becker <hi@yuri.li>
2018-09-30 23:08:58 +0200

    Added background-switch

---
+++ background-switch/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <dirent.h>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+using namespace std;
+
+int printUsage()
+{
+    cout << "Usage:" << endl <<"    background-switch <directory> <tag>";
+    return 0;
+}
+
+int printNoFilesFound()
+{
+    cout << "No files with the given tag were found." << endl;
+    return 0;
+}
+
+vector<string> getFiles(DIR* dir, char tag[])
+{
+    vector<string> files;
+
+    dirent *ent;
+    while((ent = readdir(dir)) != 0)
+    {
+        if(string(tag) == "all" || string(ent->d_name).find(tag) != string::npos )
+            files.push_back(ent->d_name);
+    }
+
+    return files;
+}
+
+string getRandomFile(vector<string> files)
+{
+    srand(time(0) ^ getpid());
+    int element = rand() % files.size();
+
+    return files[element];
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3) return printUsage();
+    char* directory = argv[1];
+    char* tag = argv[2];
+
+    DIR* dirStream = opendir(directory);
+    if(dirStream == 0) return printUsage();
+    vector<string> files = getFiles(dirStream, tag);
+    closedir(dirStream);
+
+    if(files.size() == 0) return printNoFilesFound();
+
+    string file = getRandomFile(files);
+    string fullFileDir = string(directory).append("/").append(file);
+
+    system(string("feh --bg-fill ").append(fullFileDir).c_str());
+
+    return 0;
+}
